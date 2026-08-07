d7bb2f1c329fef3146ac45898b474b5562baa148
JairoImm <jairo.imm@gmail.com>
2026-08-07 17:11:57 -0400

    fix: corregir Procfile y agregar whitenoise para Railway

--- backend/settings.py
+++ backend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,3 +158,5 @@ TRANSBANK_API_KEY = ''
 
 # URL donde tu frontend de clientes corre (para saber a dónde devolver al cliente tras pagar)
 FRONTEND_CLIENTE_URL = 'http://localhost:3001'  # ajusta cuando montemos el frontend de pedidos
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
